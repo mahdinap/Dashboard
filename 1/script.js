a155ea0911928ceab34471a40dfeca336938e144
Mahdi <mahdi1nap@gmail.com>
2025-08-06 14:05:52 +0330

    complete JS code

--- 1/script.js
+++ 1/script.js
@@ -1,5 +1,23 @@
-const switchBtn=document.querySelector(".toggle-switch")
 const body=document.querySelector("body")
-switchBtn.addEventListener("click",()=>{
-    body.classList.toggle("dark")    
+const sidebar=body.querySelector(".sidebar")
+const toggle=body.querySelector(".toggle")
+const searchBtn=body.querySelector(".search-box")
+const modeSwitch=body.querySelector(".toggle-switch")
+const modeText=body.querySelector(".mode-text")
+
+
+modeSwitch.addEventListener("click",()=>{
+    body.classList.toggle("dark") 
+    if(body.classList.contains("dark")){
+        modeText.textContent="Light Mode"
+    }   else{
+        modeText.textContent="Dark Mode"
+    }
+})
+
+toggle.addEventListener("click",()=>{
+    sidebar.classList.toggle("close")    
+})
+searchBtn.addEventListener("click",()=>{
+    sidebar.classList.remove("close")
 })
